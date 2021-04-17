65ccb80fc377df794ef514c5ee454aa9c2be6940
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-17 15:34:08 +0200

    Update L10S01.js

--- Lesson_10/L10S01.js
+++ Lesson_10/L10S01.js
@@ -3,9 +3,16 @@
 //
 var a = 0;
 var b = 1;
-gs.info(a < b);
+gs.info(a < b); //returns true or false -> result may be saved into a variable 
 
 var n = '3';
 var i = 3;
-gs.info(n == i); // REALLY?!!
+gs.info(n == i); // REALLY?!! -> == equal comparison vs. = assignment
+gs.info(n != i); // REALLY?!! -> != non-equal comparison 
+
 gs.info(i = 4); // WATCH OUT!!!
+
+var p = 5 //Integer
+var q = '5' //String
+gs.log(p == q) //compares value -> true
+gs.log(p === q) //compares value and variable type -> false 
