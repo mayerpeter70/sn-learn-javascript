a41a1dd6722e98fdd6aef4f8c9ded409a4ef9647
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-17 15:19:33 +0200

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -2,9 +2,33 @@
 //  L08S01 - Changing and detecting variable types
 //
 var i = 5;
-var iStr = i.toString();
-gs.info(typeof i);
+var iStr = i.toString(); //turns any variable into a string (numeric, Boolean)
+gs.info(typeof i); //typeOf returns the type of the specified var 
 gs.info(typeof iStr);
 
-var n = parseInt(iStr);
+var n = parseInt(iStr); //convert string into an integer
+var f = parseFloat(iStr); //convert string into a floating point number 
 gs.info(typeof n + ' n=' + n);
+
+//-----StringLab
+var dog = 'kutya'
+var cat = 'macska'
+var rat = 'patkany'
+
+gs.log(dog.length)
+gs.log(cat.length) 
+gs.log(rat.length) 
+
+var animals = dog + '\n' + cat + '\n' + rat
+gs.log(animals)
+gs.log(animals.length)
+
+//-----StringLab returns
+
+*** Script: 5
+*** Script: 6
+*** Script: 7
+*** Script: kutya
+macska
+patkany
+*** Script: 20
