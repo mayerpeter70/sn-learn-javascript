b036ee5b384729d995f29a852c7af5c03d85076a
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-18 11:22:08 +0200

    Update L12S01.js

--- Lesson_12/L12S01.js
+++ Lesson_12/L12S01.js
@@ -13,3 +13,19 @@ else
 // Introducing a shortcut way...
 var openStatusString = (valveOpen) ? 'open' : 'closed';
 gs.info('2: Valve is currently ' + openStatusString);
+
+//Evaluating string output
+var isOpenString = 'true';
+
+if (isOpenString = 'true') { // evaluation condition needs to refer to the string value, but...
+  gs.info('This is open')
+}
+
+//... we can introduce a Booelan variable using the Ternary operator
+var isOpenString = 'true';
+
+var isOpen = (isOpenString == 'true') ? true : false //short version for setting the values of isOpen Boolean using an IF statement
+
+if (isOpen) {
+  gs.info('This is open')
+}
