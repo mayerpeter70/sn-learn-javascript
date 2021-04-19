9c59a551802256264b0234861d409de5216f6055
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-19 14:31:42 +0200

    Update L14S02.js

--- Lesson_14/L14S02.js
+++ Lesson_14/L14S02.js
@@ -5,4 +5,4 @@
 // What about numbers
 //
 var num = 0; // <== try with different numbers
-gs.info(num + ' is ' + ((num) ? 'true' : 'false'));
+gs.info(num + ' is ' + ((num) ? 'true' : 'false')); // 0 and only 0 is considered to be 'false', any other number (incl. negative ones) returns 'true'
