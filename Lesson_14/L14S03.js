884d2298a6f19cb06eb5c7eafe3fe74f3bbed829
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-19 14:38:09 +0200

    Update L14S03.js

--- Lesson_14/L14S03.js
+++ Lesson_14/L14S03.js
@@ -7,6 +7,6 @@
 var string1;
 var string2 = null;
 var string3 = 'Hello, world!';
-gs.info('string1=' + ((string1) ? 'true' : 'false'));
-gs.info('string2=' + ((string2) ? 'true' : 'false'));
-gs.info('string3=' + ((string3) ? 'true' : 'false'));
+gs.info('string1=' + ((string1) ? 'true' : 'false')); // False - the variable is not defined
+gs.info('string2=' + ((string2) ? 'true' : 'false')); // False - the variable is defined as empty (null) 
+gs.info('string3=' + ((string3) ? 'true' : 'false')); //True - the variable is defined and has a value 
