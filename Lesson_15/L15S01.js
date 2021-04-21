4b4141c681cd0cc3525778e0f580f446f7afd351
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-21 10:03:05 +0200

    Update L15S01.js

--- Lesson_15/L15S01.js
+++ Lesson_15/L15S01.js
@@ -1,9 +1,9 @@
 //
 // L15S01 - Simple while loop
 //
-var i = 0;
-while (i < 5) {
-  gs.info(i);
-  i++;
+var i = 0; 
+while (i < 5) { //Check condition
+  gs.info(i); //Log current state
+  i++; //Inceremnt i
 }
-gs.info('done i=' + i);
+gs.info('done i=' + i); //Log finale state if condition is fulfilled
