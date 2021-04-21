6c078454266e555b7047a4a0130aad7ec6fb884a
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-21 11:01:56 +0200

    Update L17S01.js

--- Lesson_17/L17S01.js
+++ Lesson_17/L17S01.js
@@ -2,9 +2,9 @@
 // L17S01 - do-while
 //
 var i = 0;
-gs.info('start');
-do {
+gs.info('start');//Starting message printed 
+do { //Action: this to be carried out -> at least once this loop will run because condition is checked afterwards
   gs.info('i=' + i);
   ++i;
-} while (i < 5);
+} while (i < 5); //Condition: until this conditions IS fulfilled
 gs.info('done i=' + i);
