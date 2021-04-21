7ae0485d7109b93dbab2d60bdef3b7979abb0cc1
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-21 10:19:08 +0200

    Update L15S02.js

--- Lesson_15/L15S02.js
+++ Lesson_15/L15S02.js
@@ -2,8 +2,8 @@
 // L15S02 - breaking out of a while loop
 //
 var i = 0;
-while (true) {
-  if (i == 5)
+while (true) { //While loop executes until the test condition is true -> (true) is always true -> infinite looop...
+  if (i == 5) //... unless break stops the loop when the if condition is fulfilled
     break;
   gs.info(i);
   ++i;
