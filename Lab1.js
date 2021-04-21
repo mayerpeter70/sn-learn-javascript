a842e206e5c2e8f9bffc69617cd12ee8d59fc3b2
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-21 22:35:42 +0200

    Create Lab1.js
    Initial

---
+++ Lab1.js
@@ -0,0 +1,20 @@
+//Allocate 20 staff from four departments into 5 teams
+
+var sales = ['John', 'Anna', 'Joe', 'Lily', 'Alvin']; 
+var finance = ['Dave', 'Lia', 'Emma', 'Zoe', 'Jeff'];
+var cust_rel = ['Brad', 'Angelina', 'Mark', 'Peter', 'George'];
+var marketing = ['Lily', 'Luca', 'Amy', 'Donald', 'Derek'];
+
+var team1 = new Array() 
+var team2 = new Array() 
+var team3 = new Array() 
+var team4 = new Array() 
+var team5 = new Array() 
+
+var teams = [team1, team2, team3, team4, team5]
+
+for (i = 1; i < 6; i++) {
+  for (j = 1; j < 5; j++) {
+    teams[i] = 
+  
+  
