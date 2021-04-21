9b597ef9bb7e1e6d272d3799f31ffb20b0f66165
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-21 11:41:10 +0200

    Update L18S01.js

--- Lesson_18/L18S01.js
+++ Lesson_18/L18S01.js
@@ -1,9 +1,9 @@
 //
 // L18S01 - Nested loops
 //
-for (var i = 0; i < 5; i++) {
-  for (var j = 0; j < 3; j++) {
-    gs.info('i=' + i + ' j=' + j);
+for (var i = 0; i < 5; i++) { //Outer loop
+  for (var j = 0; j < 3; j++) { //Inner loop 
+    gs.info('i=' + i + ' j=' + j); //Value pairs of i, j logged
   }
 }
-gs.info('Done i=' + i + ' j=' + j);
+gs.info('Done i=' + i + ' j=' + j); // Final statement logged
