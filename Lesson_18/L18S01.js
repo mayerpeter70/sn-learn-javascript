837fc4ffb4efafaf06bddc2524c91e202ebf21ee
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-04-24 07:20:57 +0200

    Update L18S01.js

--- Lesson_18/L18S01.js
+++ Lesson_18/L18S01.js
@@ -7,3 +7,19 @@ for (var i = 0; i < 5; i++) { //Outer loop
   }
 }
 gs.info('Done i=' + i + ' j=' + j); // Final statement logged
+
+//Lab extended - to work on
+
+var fin = ['Laura', 'Peter', 'John', 'Anna', 'Mike']; 
+var dev = ['Zoe', 'Paul', 'Mary', 'Theresa', 'Ike'];
+var cust = ['Lily', 'Albert', 'George', 'Donald', 'Kelly'];
+var mark = ['Esther', 'Kathy', 'Louis', 'Bob', 'Bill'];
+
+for (i = 0; i < 5; i++) {
+    window("team_" + i) = new Array(fin[i], dev[i], cust[i], mark[i]);
+}
+gs.info(team_1);
+gs.info(team_2);
+gs.info(team_3);
+gs.info(team_4);
+gs.info(team_5);
