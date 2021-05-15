ba9609919bca67e960c6fd58e752b7ab7cefde89
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-05-15 07:03:22 +0200

    Update L19S03.js

--- Lesson_19/L19S03.js
+++ Lesson_19/L19S03.js
@@ -3,10 +3,10 @@
 //
 function toCelsius(fahrenheit) {
 
-    return (5 / 9) * (fahrenheit - 32);
+    return (5 / 9) * (fahrenheit - 32); //return keeps the last value in the system 
 
 }
-var c = toCelsius(32);
+var c = toCelsius(32); //declare and define var c  
 gs.info(c);
-c = toCelsius(212);
+c = toCelsius(212); //redefine var c 
 gs.info(c);
