9f60b57a80ff2621a406c7edceee38c5676d251d
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-05-15 07:12:09 +0200

    Update L19S04.js

--- Lesson_19/L19S04.js
+++ Lesson_19/L19S04.js
@@ -4,8 +4,21 @@
 function toCelsius(fahrenheit) {
 
     // c is only known in the function toCelsius()
-    var c = (5 / 9) * (fahrenheit - 32);
+    var c = (5 / 9) * (fahrenheit - 32 //c is defined in local scope, within the function only 
 
     return c;
 }
-gs.info(c); // What happened?
+gs.info(c); // here c is not known as we are outside of the function 
+
+// Solution -> define outside of the function 
+    
+function toCelsius(fahrenheit) {
+
+    // c is only known in the function toCelsius()
+    var c = (5 / 9) * (fahrenheit - 32 //c is defined in local scope, within the function only 
+
+
+    return c;
+}
+var c = toCelsius(100); //defined c outside of the function on global scope 
+gs.info(c); // now c is known and can be used 
