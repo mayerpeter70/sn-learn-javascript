cd8084d2155e078651fecd3e229830ddf0af3968
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-05-15 06:59:28 +0200

    Update L19S02.js

--- Lesson_19/L19S02.js
+++ Lesson_19/L19S02.js
@@ -1,11 +1,11 @@
 //
 // L19S02 - Function with a parameter
 //
-function toCelsius(fahrenheit) {
+function toCelsius(fahrenheit) { //farenheit is the parameter
 
     var c = (5 / 9) * (fahrenheit - 32);
 
-    gs.info(c);
+    gs.info(c); // gs.info(c + ' fok')
 }
 toCelsius(32);
 toCelsius(100);
