bec8dcf5b7c6650a9de173be54eff5fd62a0cb26
Peter Mayer <65225717+mayerpeter70@users.noreply.github.com>
2021-05-24 08:10:21 +0200

    Update L15S02.js

--- Lesson_15/L15S02.js
+++ Lesson_15/L15S02.js
@@ -6,6 +6,6 @@ while (true) { //While loop executes until the test condition is true -> (true) 
   if (i == 5) //... unless break stops the loop when the if condition is fulfilled
     break;
   gs.info(i);
-  ++i;
+  ++i; //can become infinita loop is steps 2 value (++i; ++i;) -> jumps over 5 -> safety solution is (i >= 5)!!! 
 }
 gs.info('done');
